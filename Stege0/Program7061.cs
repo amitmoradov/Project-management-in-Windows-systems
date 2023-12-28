c5bba8fdd262181263ea875367e3e947e2dec8fe
Amit <moamit21221@gmail.com>
2023-12-28 21:39:42 +0200

    new project_7061_3114

---
+++ Stege0/Program7061.cs
@@ -0,0 +1,20 @@
+﻿using System;
+//namespace Targil0;
+partial class Program7061
+
+{
+    static void Main(string[] args)
+    {
+        Welcome7061();
+        Welcome3114();
+        Console.ReadKey();
+    }
+
+    static partial void Welcome3114();
+    private static void Welcome7061()
+    {
+        Console.WriteLine("Enter your name: ");
+        string username = Console.ReadLine();
+        Console.WriteLine("{0}, welcome to my first console application", username);
+    }
+}

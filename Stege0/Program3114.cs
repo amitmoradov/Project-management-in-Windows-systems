fa8b38c31c31f4ba8b306021107e09a34cb6f62a
Yshaul2000 <yinon.shaul99@gmail.com>
2023-12-28 22:09:01 +0200

    Welcome3114()

---
+++ Stege0/Program3114.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stege0
+{
+    partial class Program
+    {
+        static partial void Welcome3114()
+        {
+            Console.WriteLine("I am also here!");
+        }
+    }
+}

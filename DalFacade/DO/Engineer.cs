b94563f353fffda20edd28825e3a72bb3a4fbd4e
Yshaul2000 <yinon.shaul99@gmail.com>
2024-01-04 22:14:36 +0200

    Discription of the recoeds + constructors .

--- DalFacade/DO/Engineer.cs
+++ DalFacade/DO/Engineer.cs
@@ -1,11 +1,23 @@
 ﻿
 namespace DO;
 
+/// <summary>
+/// Data of Engineer .
+/// </summary>
+/// <param name="Id"> Id of the engineer</param>
+/// <param name="Email"> Email of the engineer</param>
+/// <param name="Cost">Cost to hour .</param>
+/// <param name="Name">Name of the engineer .</param>
+/// <param name="Level">Level of the engineer .</param>
 public record Engineer
 (
     int Id,
-    string? Email,
-    double Cost,
-    string? Name,
-    DO.EngineerExperience Level
-);
+    double? Cost,
+    DO.EngineerExperience? Level,
+    string? Email = null,
+    string? Name = null
+    
+)
+{
+    public Engineer () : this(0, 0, null) { }
+}
--- DalFacade/DO/Task.cs
+++ DalFacade/DO/Task.cs
@@ -1,20 +1,39 @@
 ﻿
 namespace DO;
 
-
+/// <summary>
+/// The tesk of engineer .
+/// </summary>
+/// <param name="Id"> Id of the task .</param>
+/// <param name="Alise"> nickname of the task .</param>
+/// <param name="Description"> What is the task now .</param>
+/// <param name="CreatedAtDate">CreatedAtDate of the task .</param>
+/// <param name="RequiredEffortTime">RequiredEffortTime odf task .</param>
+/// <param name="IsMilestone">Milistion in the project .</param>
+/// <param name="Copmliexity"> The level of difficulty .</param>
+/// <param name="StartDate">The time to start the task .</param>
+/// <param name="ScheduledDate"> Date possible to finish</param>
+/// <param name="DeadLineDate"> Date of the finish .</param>
+/// <param name="Deliverables">Product .</param>
+/// <param name="Remarks"> Remark to the task .</param>
+/// <param name="EngineerId">The id of the charge engineer .</param>
 public record Task
 (
     int Id,
-    string? Alise,
-    string? Description,
+    int EngineerId,
     DateTime? CreatedAtDate,
     TimeSpan? RequiredEffortTime,
     bool IsMilestone,
-    DO.EngineerExperience Copmliexity,
+    DO.EngineerExperience? Copmliexity,
     DateTime? StartDate,
     DateTime? ScheduledDate,
     DateTime? DeadLineDate,
-    string? Deliverables,
-    string? Remarks,
-    int EngineerId
-);
+    string? Alisev = null,
+    string? Description = null,
+    string? Deliverables = null,
+    string? Remarks = null
+  
+)
+{
+    public Task () : this(0, 0, null, null, false, null, null, null, null) { }
+}

67475d4923c9aa4a51cb4de05bf4cb8c7e51ec2a
Amit <moamit21221@gmail.com>
2024-01-05 12:16:30 +0200

    w

--- DalFacade/DO/Dependency.cs
+++ DalFacade/DO/Dependency.cs
@@ -7,16 +7,11 @@ namespace DO;
 /// <param name="DependentTask">Id of the DependentTask .</param>
 /// <param name="DependsOnTask">Id of the previous tesk .</param>
 public record Dependency
-
 (
     int Id,
     int DependentTask,
     int DependsOnTask
 )
 {
-    /// <summary>
-    /// Empty ctor
-    /// </summary>
-public Dependency() : this (0,0,0) { }
+    public Dependency(): this(0, 0, 0) { }
 }
-

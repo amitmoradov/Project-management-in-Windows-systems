ed9a4e72249a1e05996245f1fd0ea1f848c4ac99
Amit <moamit21221@gmail.com>
2024-01-14 21:55:55 +0200

    start the DalList class

---
+++ dotNet5784_7061_3114/DalList/DalList.cs
@@ -0,0 +1,15 @@
+﻿namespace Dal;
+using DalApi;
+
+/// <summary>
+/// sealed : The class cannot be inherited
+/// </summary>
+sealed public class DalList : IDal
+{
+    public IDependency Dependency => new DependencyImplementation();
+
+    public IEngineer Engineer =>  new EngineerImplementation();
+
+    public ITask Task => new TaskImplementation();
+}
+

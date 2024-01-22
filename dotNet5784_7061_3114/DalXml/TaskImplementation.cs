0ae1253fba9710fd87a817f70bdb7e994b3713f9
Yshaul2000 <yinon.shaul99@gmail.com>
2024-01-23 00:30:04 +0200

    Finish task impementation .

--- dotNet5784_7061_3114/DalXml/TaskImplementation.cs
+++ dotNet5784_7061_3114/DalXml/TaskImplementation.cs
@@ -3,38 +3,122 @@ using DalApi;
 using DO;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 internal class TaskImplementation : ITask
 {
-
     readonly string e_task_xml = "tasks";
+    readonly string data_config_xml = "data-config";
+
     public int Create(Task item)
     {
-        throw new NotImplementedException();
+        // Load XML file to the list 
+        List<Task> tasks = XMLTools.LoadListFromXMLSerializer<Task>(e_task_xml);
+
+        //Chack if he exist
+        Task? task = Read(item._id);
+
+        if (task == null)
+        {
+            // Get the run number id .
+            int new_id = XMLTools.GetAndIncreaseNextId(data_config_xml, e_task_xml);
+
+            // Create new task with the new id .
+            Task copy_item = item with { _id = new_id };
+            // Add the update to new list
+            tasks.Add(copy_item);
+
+            // Update the xml file after change of list engineer.
+            XMLTools.SaveListToXMLSerializer<Task>(tasks, e_task_xml);
+            return new_id;
+        }
+        // If the object is exist
+        throw new DalDoesExistException($"Task with ID = {item._id} is exists");
     }
 
     public void Delete(int id)
     {
-        throw new NotImplementedException();
+        // Get the file content .
+        List<Task> tasks = XMLTools.LoadListFromXMLSerializer<Task>(e_task_xml);
+        Task? task_to_delete = Read(id);
+
+        // The object can to remove
+        if (task_to_delete is not null && task_to_delete._canToRemove)
+        {
+            tasks.Remove(task_to_delete);
+
+            // Update the xml file after change of list engineer.
+            XMLTools.SaveListToXMLSerializer<Task>(tasks, e_task_xml);
+            return;
+        }
+        if (task_to_delete is not null && !task_to_delete._canToRemove)
+        {
+            throw new DalCannotDeleted($"Task with ID = {id} cannot be deleted");
+        }
+        throw new DalDoesNotExistException($"Task with ID = {id} is Not exists");
     }
 
     public Task? Read(int id)
     {
-        throw new NotImplementedException();
+        // Get the file content .
+        List<Task> tasks = XMLTools.LoadListFromXMLSerializer<Task>(e_task_xml);
+
+        if (tasks is not null)
+        {
+            // Pass all DataBase and find the task that answer the requirement
+            Task? requested_Task = tasks.Find(item => item._id == id);
+            return requested_Task;
+        }
+        return null;
     }
 
     public Task? Read(Func<Task, bool> filter)
     {
-        throw new NotImplementedException();
+        // Get the file content .
+        List<Task> tasks = XMLTools.LoadListFromXMLSerializer<Task>(e_task_xml);
+
+        if (tasks is not null)
+        {
+            // Pass all DataBase and find the engineer that answer the requirement
+            return tasks.FirstOrDefault(filter);
+        }
+        return null;
     }
 
     public IEnumerable<Task?> ReadAll(Func<Task, bool>? filter = null)
     {
-        throw new NotImplementedException();
+        // Get the file content .
+        List<Task> tasks = XMLTools.LoadListFromXMLSerializer<Task>(e_task_xml);
+
+        if (filter != null)
+        {
+            return from item in tasks
+                   where filter(item)
+                   select item;
+        }
+
+        //Filter == null - return all elements.
+        return from item in tasks
+               select item;
     }
 
     public void Update(Task item)
     {
-        throw new NotImplementedException();
+        List<Task> tasks = XMLTools.LoadListFromXMLSerializer<Task>(e_task_xml);
+
+        if (item is not null)
+        {
+            //Delete the old engineer with same id
+            Delete(item._id);
+            // Add the update to new list
+            tasks.Add(item);
+
+            // Update the xml file after change of list engineer.
+            XMLTools.SaveListToXMLSerializer<Task>(tasks, e_task_xml);
+            return;
+        }
+        throw new DalDoesNotExistException($"Task with ID = {item!._id} is Not exists");
     }
 }
+
+

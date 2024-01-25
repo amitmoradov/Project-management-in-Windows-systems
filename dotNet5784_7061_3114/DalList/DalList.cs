35988cdb4eda682110e9ceb2e0dea82379326a69
Amit <moamit21221@gmail.com>
2024-01-25 18:14:37 +0200

    .

--- dotNet5784_7061_3114/DalList/DalList.cs
+++ dotNet5784_7061_3114/DalList/DalList.cs
@@ -4,8 +4,11 @@ using DalApi;
 /// <summary>
 /// sealed : The class cannot be inherited
 /// </summary>
-sealed public class DalList : IDal
+sealed internal class DalList : IDal
 {
+    public static IDal Instance { get; } = new DalList();
+    private DalList() { }
+
     public IDependency Dependency => new DependencyImplementation();
 
     public IEngineer Engineer =>  new EngineerImplementation();
--- dotNet5784_7061_3114/DalXml/DalXml.cs
+++ dotNet5784_7061_3114/DalXml/DalXml.cs
@@ -1,8 +1,12 @@
 ﻿using DalApi;
+using System.Diagnostics;
 namespace Dal;
 
-public class DalXml : IDal
+sealed internal class DalXml : IDal
 {
+    public static IDal Instance { get; } = new DalXml();
+    private DalXml() { }
+
     public IDependency Dependency => new DependencyImplementation();
 
     public IEngineer Engineer => new EngineerImplementation();

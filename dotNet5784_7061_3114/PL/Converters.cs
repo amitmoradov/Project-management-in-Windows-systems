b99e3038d67d4a6f4949f6d298b49d73ea384e2c
Amit <moamit21221@gmail.com>
2024-04-01 16:29:29 +0300

    .

--- dotNet5784_7061_3114/PL/Converters.cs
+++ dotNet5784_7061_3114/PL/Converters.cs
@@ -220,6 +220,21 @@ public class ConvertDateTimeToInt : IValueConverter
     }
 }
 
+class AccsessTCreateDate : IValueConverter
+{
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return (int)value == 0;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+
+}
+
 public class ConvertStatusTaskGantt : IValueConverter
 {
     static readonly BlApi.IBl e_bl = BlApi.Factory.Get();
